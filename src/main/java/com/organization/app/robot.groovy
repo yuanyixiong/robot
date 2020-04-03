3ba06dc7c2017b6026fca5e885592d52ee3d8ca7
袁毅雄 <15926499574@163.com>
2020-04-03 22:22:48 +0800

    git 钩子

--- src/main/java/com/organization/app/robot.groovy
+++ src/main/java/com/organization/app/robot.groovy
@@ -57,7 +57,7 @@ if (gitblit.getBoolean(Keys.web.mountParameters, true)) {
 def commitCount = 0
 def changes = ''
 SimpleDateFormat df = new SimpleDateFormat(gitblit.getString(Keys.web.datetimestampLongFormat, 'yyyy-MM-dd hh:mm:ss'))
-def table = { "认证信息：${JGitUtils.getDisplayName(it.authorIdent)}\n时间：${df.format(JGitUtils.getCommitDate(it))}\n描述：$it.shortMessage\n" }
+def table = { "\n提交者：${JGitUtils.getDisplayName(it.authorIdent)}\n提交时间：${df.format(JGitUtils.getCommitDate(it))}\n提交描述：$it.shortMessage\n提交版本：$it.id.name\n" }
 for (command in commands) {
     def ref = command.refName
     def refType = 'branch'
@@ -72,26 +72,23 @@ for (command in commands) {
         case ReceiveCommand.Type.CREATE:
             def commits = JGitUtils.getRevLog(r, command.oldId.name, command.newId.name).reverse()
             commitCount += commits.size()
-            changes += "new $refType $ref created ($commits.size commits)\n"
+            changes += "资源：$ref \n类型：$refType \n操作：created ($commits.size commits)\n"
             changes += commits.collect(table)
-            changes += '\n'
             break
         case ReceiveCommand.Type.UPDATE:
             def commits = JGitUtils.getRevLog(r, command.oldId.name, command.newId.name).reverse()
             commitCount += commits.size()
-            changes += "$ref $refType updated ($commits.size commits)\n"
+            changes += "资源：$ref \n类型：$refType \n操作：updated ($commits.size commits)\n"
             changes += commits.collect(table)
-            changes += '\n'
             break
         case ReceiveCommand.Type.UPDATE_NONFASTFORWARD:
             def commits = JGitUtils.getRevLog(r, command.oldId.name, command.newId.name).reverse()
             commitCount += commits.size()
-            changes += "$ref $refType updated [NON fast-forward] ($commits.size commits)\n"
+            changes += "资源：$ref \n类型：$refType \n操作：updated [NON fast-forward] ($commits.size commits)\n"
             changes += commits.collect(table)
-            changes += '\n'
             break
         case ReceiveCommand.Type.DELETE:
-            changes += "$ref $refType deleted\n"
+            changes += "资源：$ref \n类型：$refType \n操作：deleted"
             break
         default:
             break
@@ -99,20 +96,19 @@ for (command in commands) {
 }
 r.close()
 
-
 def timestamp = System.currentTimeMillis();
-def secret = "SEC8ea1b9174903d0047634543b8f604421e10b68343d4991dd321aebefd675e559";
+def secret = "SECf479ce148f10199133698c39e9f7776c337742c0efbe53dda7b4396836b547ab";
 def stringToSign = timestamp + "\n" + secret;
 def mac = Mac.getInstance("HmacSHA256");
 mac.init(new SecretKeySpec(secret.getBytes("UTF-8"), "HmacSHA256"));
 def signData = mac.doFinal(stringToSign.getBytes("UTF-8"));
 def sign = URLEncoder.encode(new String(Base64.encodeBase64(signData)),"UTF-8");
-def connection = new URL("https://oapi.dingtalk.com/robot/send?access_token=c4af8f3307c51e593fc235db99ea24fe62f560bd7afa1b11d9d1346babbc7320&timestamp="+timestamp+"&sign="+sign).openConnection()
+def connection = new URL("https://oapi.dingtalk.com/robot/send?access_token=562675fc20be1ad0d46c8ecd1199ea7db3a368106c6d3579da87568c50bc6ff0&timestamp="+timestamp+"&sign="+sign).openConnection()
 connection.setRequestMethod('POST')
 connection.doOutput = true
 connection.setRequestProperty("Content-Type", "application/json")
 def writer = new OutputStreamWriter(connection.outputStream,"utf-8")
-def content ="{\"msgtype\": \"text\",\"text\": {\"content\": \""+"提交者：$user.username\n提交数量：$commitCount\n版本库：$repository.name"+"$summaryUrl\n版本：$it.id.name\n$changes"+" \"},\"at\": {\"atMobiles\": [],\"isAtAll\": false}}"
+def content ="{\"msgtype\": \"text\",\"text\": {\"content\": \""+"推送者：$user.username\n推送数量：$commitCount\n版本库：$repository.name"+"$summaryUrl\n$changes"+" \"},\"at\": {\"atMobiles\": [],\"isAtAll\": true}}"
 writer.write(content.toString())
 writer.flush()
 writer.close()
